fd002ff81af86432f44372f79daed4a68887d9ff
Plumblumpb <2573350997@qq.com>
2019-04-24 10:20:50 +0800

    rocketmq

--- rocketMq/src/main/java/producer/Producer.java
+++ rocketMq/src/main/java/producer/Producer.java
@@ -4,7 +4,6 @@ import org.apache.rocketmq.client.producer.DefaultMQProducer;
 
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
-import org.apache.rocketmq.remoting.common.RemotingHelper;
 
 /**
  * @Auther: cpb
@@ -14,8 +13,7 @@ import org.apache.rocketmq.remoting.common.RemotingHelper;
 public class Producer {
     public static void main(String[] args) {
         DefaultMQProducer producer = new DefaultMQProducer("Producer");
-        //不知道为什么写172.0.0.1不行写localhost可以
-        producer.setNamesrvAddr("localhost:9876");
+        producer.setNamesrvAddr("127.0.0.1:9876");
         //设置自动创建topic的key值
         producer.setCreateTopicKey("AUTO_CREATE_TOPIC_KEY");
         try {
